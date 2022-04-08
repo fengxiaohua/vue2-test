61cdef6f6e3ee7c84b8a2f0da79008834516bed2
xiaohang <472843286@qq.com>
2022-04-08 16:51:44 +0800

    首页提交测试

---
+++ src/pages/home/index.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Description: 首页
+ * @Date: 2022-04-08 16:50:01
+-->
+<template>
+  <div>
+    <h1>hello Vue 项目测试</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
