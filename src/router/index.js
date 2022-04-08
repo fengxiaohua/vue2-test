ba7baa7c7fa780a536f557689536525ff8132f4f
xiaohang <472843286@qq.com>
2022-04-08 16:51:44 +0800

    首页提交测试

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
+/*
+ * @Description: 路由设置
+ * @Date: 2022-04-08 13:54:28
+ */
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home'
 
 Vue.use(Router)
 
@@ -9,7 +13,7 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: Home
     }
   ]
 })
