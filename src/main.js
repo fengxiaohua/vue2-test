19b702b7f66fb1a2abe7a2f171a6341d371ace7b
xiaohang <472843286@qq.com>
2022-04-22 17:06:48 +0800

    1. 登录静态页面;2. eslint校验添加;3. 添加包

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
+/*
+ * @Description:
+ * @Date: 2022-04-12 14:43:53
+ */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 引入ant-design-vue
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
 Vue.config.productionTip = false
+
+Vue.use(Antd)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
